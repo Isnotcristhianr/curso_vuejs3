1c02f3900931892d756263efee6335af5c0c9ec2
Cristhian David Recalde <crecalde@neaternotes.com>
2024-12-11 19:52:33 -0500

    formulario

--- 04_formularios/src/components/Formulario.vue
+++ 04_formularios/src/components/Formulario.vue
@@ -1,16 +1,65 @@
+<script>
+import Table from "./Table.vue";
+
+export default {
+  components: {
+    Table,
+  },
+  data: () => ({
+    nombre: "",
+    control: "",
+    fecha: "",
+    comentario: "",
+    registros: [],
+  }),
+  methods: {
+    enviarFormulario() {
+      console.log("Formulario enviado");
+      this.registros.push({
+        nombre: this.nombre,
+        control: this.control,
+        fecha: this.fecha,
+        comentario: this.comentario,
+      });
+      console.log(this.registros);
+      this.limpiarFormulario();
+    },
+    limpiarFormulario() {
+      this.nombre = "";
+      this.control = "";
+      this.fecha = "";
+      this.comentario = "";
+    },
+  },
+};
+</script>
+
 <template>
   <div class="row">
     <div class="col-12">
       <hr />
       <h3 class="mb-4">Papeleria</h3>
-      <form action="" @submit.prevent="" class="needs-validation">
+      <form @submit.prevent="enviarFormulario" class="needs-validation">
         <div class="mb-3">
           <label for="nombre" class="form-label">Nombre del Producto</label>
-          <input type="text" name="nombre" id="nombre" class="form-control" />
+          <input
+            v-model="nombre"
+            type="text"
+            name="nombre"
+            id="nombre"
+            class="form-control"
+            required
+          />
         </div>
         <div class="mb-3">
           <label for="control" class="form-label">Cantidad de productos</label>
-          <select name="control" id="control" class="form-select">
+          <select
+            v-model="control"
+            name="control"
+            id="control"
+            class="form-select"
+            required
+          >
             <option value="1">1</option>
             <option value="2">2</option>
             <option value="3">3</option>
@@ -18,19 +67,32 @@
         </div>
         <div class="mb-3">
           <label for="fecha" class="form-label">Fecha de entrega</label>
-          <input type="date" name="fecha" id="fecha" class="form-control" />
+          <input
+            v-model="fecha"
+            type="date"
+            name="fecha"
+            id="fecha"
+            class="form-control"
+            required
+          />
         </div>
         <div class="mb-3">
           <label for="comentario" class="form-label">Comentario</label>
           <textarea
+            v-model="comentario"
             name="comentario"
             id="comentario"
             class="form-control"
             rows="3"
+            required
           ></textarea>
         </div>
-        <button type="submit" class="btn btn-primary w-100">Enviar</button>
+        <button type="submit" class="btn btn-primary w-100 m-2">Enviar</button>
       </form>
     </div>
+    <!-- Tabla -->
+    <Table
+        :registros="registros"
+    />
   </div>
 </template>
---
+++ 04_formularios/src/components/Table.vue
@@ -0,0 +1,32 @@
+<script>
+export default {
+  props: {
+    registros: {
+      type: Array,
+      required: true,
+      default: () => []
+    }
+  },
+};
+</script>
+
+<template>
+  <table class="table">
+    <thead>
+      <tr>
+        <th>Nombre</th>
+        <th>Cantidad</th>
+        <th>Fecha</th>
+        <th>Comentario</th>
+      </tr>
+    </thead>
+    <tbody>
+        <tr v-for="registro in registros" :key="registro.id">
+            <td>{{ registro.nombre }}</td>
+            <td>{{ registro.control }}</td>
+            <td>{{ registro.fecha }}</td>
+            <td>{{ registro.comentario }}</td>
+        </tr>
+    </tbody>
+  </table>
+</template>
