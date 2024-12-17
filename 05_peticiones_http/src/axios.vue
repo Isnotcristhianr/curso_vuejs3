15d06b68b5d7f8d0275adf7bab847c5bbf51a8b1
Cristhian David Recalde <crecalde@neaternotes.com>
2024-12-16 19:25:21 -0500

    Add Axios integration for API data fetching in axios.vue; update dependencies in package.json and package-lock.json

--- 05_peticiones_http/src/axios.vue
+++ 05_peticiones_http/src/axios.vue
@@ -1,8 +1,30 @@
-<script setup>
+<script>
+import axios from 'axios';
+const url = "https://api.coindesk.com/v1/bpi/currentprice.json";
+
+export default {
+    data() {
+        return {
+            respuesta: null
+        }
+    },
+    methods: {
+        axiosGet() {
+            axios.get(url)
+                .then(res => this.respuesta = res.data)
+                .catch(err => console.log(err))
+        }
+    }
+}
+
 </script>
 
 <template>
   <div>
     <h1>Axios</h1>
+    <button @click="axiosGet">
+      Obtener Datos
+    </button>
+    <pre>{{ respuesta }}</pre>
   </div>
 </template>
