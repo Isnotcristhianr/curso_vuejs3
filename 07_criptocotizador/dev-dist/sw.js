647cfafaa1bba164485fc7981ddb83868ad00cc2
Cristhian David Recalde <crecalde@neaternotes.com>
2025-01-07 18:43:41 -0500

    Update service worker cache revision for index.html in sw.js

--- 07_criptocotizador/dev-dist/sw.js
+++ 07_criptocotizador/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.u6ri1k62aeg"
+    "revision": "0.aplrq8tgf0g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
