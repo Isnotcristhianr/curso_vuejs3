16aa6616ee409fad55e441c4361b5ae4f63ee573
Cristhian David Recalde <crecalde@neaternotes.com>
2025-02-03 19:08:43 -0500

    integrate vue fire

--- 11_Instagram/src/main.ts
+++ 11_Instagram/src/main.ts
@@ -6,9 +6,19 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './presentation/router'
 
+//firebase
+import { VueFire, VueFireAuth } from 'vuefire'
+import { FirebaseApp } from './config/firebaseConfig'
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+
+//firebase
+app.use(VueFire, {
+  firebaseApp: FirebaseApp,
+  modules: [VueFireAuth()],
+})
 
 app.mount('#app')
